bdd638e2c6e4a924e4cb131f55f3da8158016f3c
Diego Quipuzco <demential2003@gmail.com>
2025-02-28 14:04:56 -0500

    adición de animaciones a las cards de inicio

--- src/components/index/Card.tsx
+++ src/components/index/Card.tsx
@@ -6,19 +6,21 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ title, image, paragraph }) => {
   return (
-    <div className="relative w-11/12 h-3/4 lg:w-2/3 lg:h-4/5 flex hover:scale-105 transition-all">
+    <div className="relative w-11/12 h-3/4 lg:w-4/6 lg:h-4/5 flex hover:scale-105 duration-300 transition-all group">
       <img
         src={image}
         alt={`fondo de la tarjeta ${title}`}
         className="absolute rounded-3xl w-full h-full object-cover object-center z-0"
       />
-      <div className="relative self-end z-10 text-white w-full h-full lg:h-2/3 flex flex-col justify-end p-4 text-center bg-gradient-to-t from-teal-800 rounded-3xl">
-        <h3 className="font-semibold text-lg lg:text-base 2xl:text-4xl mb-0 md:mb-1">
-          {title}
-        </h3>
-        <p className="font-light text-lg lg:text-sm 2xl:text-3xl lg:whitespace-break-spaces">
-          {paragraph}
-        </p>
+      <div className="relative self-end z-10 text-white w-full h-full flex flex-col justify-end p-4 pb-0 group-hover:pb-4 text-center bg-gradient-to-t from-teal-800 rounded-3xl">
+        <div className="translate-y-12 group-hover:-translate-y-1 transition-transform duration-300">
+          <h3 className="font-semibold text-lg lg:text-base 2xl:text-4xl mb-0 md:mb-1 ">
+            {title}
+          </h3>
+          <p className="opacity-0 group-hover:opacity-100 transition-opacity duration-700 ease-in-out font-light text-lg lg:text-sm 2xl:text-3xl lg:whitespace-break-spaces">
+            {paragraph}
+          </p>
+        </div>
       </div>
     </div>
   );
