8139bbe228af776cc733ffe15236a91274eadfe5
Diego Quipuzco <demential2003@gmail.com>
2025-03-05 13:50:23 -0500

    cards de valores en About

---
+++ src/data/valores.data.ts
@@ -0,0 +1,28 @@
+import imgResponsabilidad from "@images/about/card_responsabilidad.webp";
+import imgCalidad from "@images/about/card_calidad.webp";
+import imgTranspariencia from "@images/about/card_transpariencia.webp";
+import imgCompromiso from "@images/about/card_compromiso.webp";
+
+const valoresArray = [
+  {
+    image: imgResponsabilidad.src,
+    title: "RESPONSABILIDAD",
+    paragraph: "Con nuestros empleados y la sociedad.",
+  },
+  {
+    image: imgCalidad.src,
+    title: "CALIDAD DE SERVICIO",
+    paragraph: "Nuestra característica principal.",
+  },
+  {
+    image: imgTranspariencia.src,
+    title: "TRANSPARIENCIA",
+    paragraph: "En nuestras acciones.",
+  },
+  {
+    image: imgCompromiso.src,
+    title: "COMPROMISO",
+    paragraph: "En ofrecer la máxima calidad en nuestros productos.",
+  },
+];
+export default valoresArray;
