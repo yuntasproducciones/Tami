44808d3f254051bcbeb1dc128be52e06201af0ee
Diego Quipuzco <demential2003@gmail.com>
2025-03-10 12:48:07 -0500

    Página de productos experimental implementada

---
+++ src/data/heroProducts.data.ts
@@ -0,0 +1,43 @@
+import slide1 from "@images/products/hero_productos.webp";
+import slide2 from "@images/products/selladora_vasos.webp";
+import slide3 from "@images/products/soldadora_spark.webp";
+import slide4 from "@images/products/ventilador_holografico.webp";
+import slide5 from "@images/products/selladora_botellas.webp";
+
+const heroProducts = [
+  {
+    image: slide1.src,
+    title: "NUESTROS\nPRODUCTOS",
+    items: [],
+  },
+  {
+    image: slide2.src,
+    subTitulo1: "EMPRENDIMIENTO",
+    title: "SELLADORA\nDE VASOS",
+    subTitulo2: "Los derrames jamás volverán\na ser un problema",
+    items: [],
+  },
+  {
+    image: slide3.src,
+    subTitulo1: "TRABAJO",
+    title: "SOLDADORA\nSPARK",
+    subTitulo2:
+      "Potencia, versatilidad y resultados\nde calidad con la soldadura SPARK",
+    items: [],
+  },
+  {
+    image: slide4.src,
+    subTitulo1: "DECORACIÓN",
+    title: "VENTILADOR\nHOLOGRÁFICO",
+    subTitulo2: "Impulsa tu marca con\nnuestros hologramas",
+    items: [],
+  },
+  {
+    image: slide5.src,
+    subTitulo1: "EMPRENDIMIENTO",
+    title: "SELLADORA\nDE BOTELLAS",
+    subTitulo2: "Sella, protege y asegura\ncada botella con presión.",
+    items: [],
+  },
+];
+export default heroProducts;
---
+++ src/models/Product.ts
@@ -0,0 +1,7 @@
+export default interface Producto {
+  id: string;
+  nombreProducto: string;
+  stockProducto: number;
+  precioProducto: number;
+  image: string;
+}
