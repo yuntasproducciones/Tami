b806d6aa05614cd23f15569f56f453b1595f7993
Diego Quipuzco <demential2003@gmail.com>
2025-03-10 12:48:57 -0500

    rename a archivo

---
+++ src/data/sectionCards.data.ts
@@ -0,0 +1,25 @@
+import bg_card1 from "@images/index/cards/card_trabajo.webp";
+import bg_card2 from "@images/index/cards/card_decoracion_y_hogar.webp";
+import bg_card3 from "@images/index/cards/card_negocio.webp";
+const sectionCards = [
+  {
+    title: "TRABAJO",
+    paragraph:
+      "Equipos diseñados para\noptimizar y agilizar tus\nprocesos laborales.",
+    image: bg_card1,
+  },
+  {
+    title: "DECORACIÓN Y HOGAR",
+    paragraph:
+      "Productos que combinan\nfuncionalidad y estilo para\ntu espacio.",
+    image: bg_card2,
+  },
+  {
+    title: "NEGOCIO",
+    paragraph:
+      "Herramientas innovadoras\nque impulsan el crecimiento\nde tu negocio.",
+    image: bg_card3,
+  },
+];
+
+export default sectionCards;
