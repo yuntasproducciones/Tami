84dbd3208bcaaf218d47ffd44ddff90323829568
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2025-03-11 09:29:13 -0500

    agregar alias de rutas en la configuración de Vite para facilitar la importación de módulos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import path from 'path';
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
-        plugins: [tailwindcss()]
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@images': path.resolve('./src/assets/images'),
+                '@components': path.resolve('./src/components'),
+                '@layouts': path.resolve('./src/layouts')
+                // Añade más alias según sea necesario para tu proyecto
+            }
+        }
     },
     integrations: [react()]
 });
