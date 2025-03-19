295b5c7e05fe697ae9bb276e7fa8d449635c91d1
Diego Quipuzco <demential2003@gmail.com>
2025-03-19 12:38:45 -0500

    product Page implementada

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,8 +1,17 @@
+import type Dimensions from "./Dimensions";
+import type Specs from "./Specs";
+
 export default interface Producto {
   id: string;
-  nombreProducto: string;
-  stockProducto: number;
-  precioProducto: number;
+  title: string;
+  subtitle: string;
+  tagline: string;
+  description: string;
+  specs: Specs;
+  dimensions: Dimensions;
+  relatedProducts: string[];
+  images: string[];
   image: string;
+  nombreProducto: string;
   seccion: string;
 }
